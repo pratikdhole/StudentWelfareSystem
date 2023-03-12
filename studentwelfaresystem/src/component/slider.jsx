76a4126504cd3a67bf4cd466f6bf3f27060f23f8
unknown <pratik.d2011@gmail.com>
2023-03-12 17:30:07 +0530

    property Ui

--- studentwelfaresystem/src/component/slider.jsx
+++ studentwelfaresystem/src/component/slider.jsx
@@ -1,23 +1,143 @@
 import React, { useState } from 'react';
 import { Slider } from 'react-bootstrap';
-
+import "../assets/slider.css"
 const Sliderb = () => {
-  const [sliderValue, setSliderValue] = useState(50);
+    const [sliderValue, setSliderValue] = useState(50);
 
-  return (
-    <div>
-      <Slider
-        value={sliderValue}
-        onChange={(value) => setSliderValue(value)}
-        min={0}
-        max={100}
-        step={1}
-        tooltip="auto"
-        tooltipPlacement="top"
-      />
-      <p>{sliderValue}</p>
-    </div>
-  );
+    return (
+        <div>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-1-card-1.jpg" alt="..." />
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-1-card-2.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-1-card-3.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-2-card-1.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-2-card-2.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-2-card-3.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/bootstrap-multiple-items-carousel-slide-3-card-1.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-3-card-2.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="image-wrapper">
+                                    <img src="https://codingyaar.com/wp-content/uploads/multiple-items-carousel-slide-3-card-3.jpg" alt="..."/>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                        content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <p class="mt-5 text-center">Get a step-by-step written explanation here: <a href="https://codingyaar.com/bootstrap-4-carousel-multiple-items-responsive/" target="_blank">Bootstrap Carousel Card Slider
+            </a> </p>
+
+            <p class="mt-3 text-center">Get a step-by-step video explanation here: <a href="https://youtu.be/kHPm_AlxP7U" target="_blank">Bootstrap Carousel Card Slider
+            </a> 
+            </p>
+        </div>
+    );
 };
 
 export default Sliderb;
--- studentwelfaresystem/src/pages/ViewPG.jsx
+++ studentwelfaresystem/src/pages/ViewPG.jsx
@@ -17,9 +17,9 @@ const ViewPG = () => {
       name: "Atharv PG",
       address: "Pashan, Pune",
       image: room1
-    }
-
-      image: room1,
+    },
+    {
+      image: room2,
       price: 15000,
       size: "1200 sqft",
       bathrooms: 2,
@@ -48,17 +48,17 @@ const ViewPG = () => {
   ]);
 
   return (
-    <div className="row">
+    <div className="d-flex container mb-5">
 
       {/* <div className="col-md-2 filter-container bg-dark justify-content-center">
         filter
       </div> */}
 
-      <div className="col-md-2 filter-container bg-dark justify-content-center">
+      <div className="col-md-3 filter-container mt-3 justify-content-center">
         filter
       </div>
 
-      <div className="col-md-10 property-listing">
+      <div className="col-md-9 property-listing">
         {properties.map((property) => (
           <PropertyCard key={property.name} {...property} />
         ))}
@@ -70,9 +70,6 @@ const ViewPG = () => {
 const PropertyCard = ({
   name,
   address,
-
-  image
-
   image,
   price,
   size,
@@ -81,126 +78,72 @@ const PropertyCard = ({
 
 }) => {
   return (
-    <div className="col-md-8 view-pg">
-      <div className="pg-name">
-
-        <h2>Home Name</h2>
-        <h4>Address</h4>
-      </div>
-      <div className="pg-details">
-        <div className="row">
-          <div className="col-md-4 p-1 border rounded">
-            <div className="">Rent</div>
-          </div>
-          <div className="col-md-4 p-1 border rounded">
-            <div className="">Sharing Type</div>
-          </div>
-          <div className="col-md-4 p-1 border rounded">
-            <div className="">Gender</div>
-          </div>
+    <div>
+      <div className="col-md-10 view-pg-section shadow-defaultCardShadow mt-3">
+        <div className="pg-name">
+          <h2>3 BHK Flat  In Gangotri Grandeur, For Sale  In Kormanagla</h2>
+          <h4>4th block 8th A main</h4>
         </div>
-      </div>
-      <div className="pg-description">
-        <div className="photo-gallery col-md-4">
-
-        <h1>Home Name</h1>
-        <h3>Address</h3>
-      </div>
-      <div className="pg-details">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="card">A</div>
-          </div>
-          <div className="col-md-4">
-            <div className="card">A</div>
-          </div>
-          <div className="col-md-4">
-            <div className="card">A</div>
-          </div>
-        </div>
-      </div>
-      <div className="pg-discrption">
-        <div className="photo-gallry col-md-4">
-
-          <div className="card">
-            <img src={image} alt={name} />
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="row">
-            <div className="amenities">
-              <img src={wifi} width="10%" alt={wifi} />&nbsp;&nbsp;Wifi
-              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              | Yes</span>
+        <div className="pg-amount border-cardbordercolor d-flex ">
+          <div className="col-md-4 text-center">
+            <div className="font-semi-bold fontsme">
+              2.5 Crores
             </div>
-            <div className="amenities">
-              <img src={washingmachine} width="10%" alt={washingmachine} /> Laundry
-              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Yes</span>
-            </div>
-            <div className="amenities">
-              <img src={furniture} width="10%" alt={furniture} />&nbsp;Furniture
-              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Yes</span>
-            </div>
-            <div className="amenities">
-              <img src={waterfilter} width="10%" alt={waterfilter} />&nbsp;&nbsp;Water Filter
-              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Yes</span>
+            <div className="dpname">
+              Deposit
             </div>
           </div>
-            <Container className="text-center mb-2 py-2">
-              <button type="button" class="btn btn-outline-secondary">Get Owner Details</button>
-            </Container>
-          {/* <div>
-            <button type="button" class="btn btn-outline-info">Get Owner Details</button>
-          </div> */}
-        </div>
-      </div>
-      <div>
-      </div>
-    </div>
-
-        <div className="col-md-8">
-        <div className="row">
-          <div className="col-md-6">A</div>
-          <div className="col-md-6">A</div>
-          <div className="col-md-6">A</div>
-          <div className="col-md-6">A</div>
-        </div>
-        </div>
-     
+          <div className="col-md-4 text-center">
+            <div className="font-semi-bold fontsme">
+              Shared
+            </div>
+            <div className="dpname">
+              Room Type Available
+            </div>
           </div>
-          <div className="pg-owner-details">
-          <p>test area</p>
+          <div className="col-md-4 text-center">
+            <div className="font-semi-bold fontsme">
+              ₹3,500
+            </div>
+            <div className="dpname">
+              Rent/Month
+            </div>
+          </div>
         </div>
-    </div>
+        <div className="pg-details-section d-flex p-3">
 
-    // <div className="col-md-5 property-card text-justify">
-    //     <img src={image} alt={name} />
-    //     <div className="property-details">
-    //         <h1>{name}</h1>
-    //         <h5>{address}</h5>
-    //         <h4>Rs.{price}/month</h4>
-    //     </div>
-    //     <div className='property-amenities'>
-    //         <div className='property-amenity-list-1'>
-    //             <div className='amenity-box'>
-    //                 Washing Machine
-    //             <img src={washingmachine}/>
-    //             </div>
-    //             <div className='amenity-box'>Filter
-    //             <img src={waterfilter}/></div>
-    //         </div>
-    //         <div className='property-amenity-list-1'>
-    //             <div className='amenity-box'>Wifi
-    //             <img src={wifi}/></div>
-    //             <div className='amenity-box'>Furniture
-    //             <img src={furniture} /></div>
-    //         </div>
-    //     </div>
-    //     <div className='mr-md-3 contact-box'>
-    //         <button type="submit" className="btn btn-success mt-2 ">Get contact details</button>
-    //     </div>
-    // </div>
+          <div className="photo-section">
+            <img src={image} />
+          </div>
+          <div className="dis-img">
+            <div className="dis-img-section">
+              <div className="discrption-flow">
+                <div class="py-0 pr-0.5p pl-3p bg-left">
+
+                </div>
+                <div class="flex"><div><div class="font-semibold">Any</div>
+                  <div class="">
+                    <div class="heading-7 text-default-color mt-0.5p">Preferred Tenants</div>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <div className="discrption-flow">
+                <div class="py-0 pr-0.5p pl-3p bg-left">
+
+                </div>
+                <div class="flex"><div><div class="font-semibold">Any</div>
+                  <div class="">
+                    <div class="heading-7 text-default-color mt-0.5p">Preferred Tenants</div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+</div>
+   </div>
+   </div>
   );
 };
 
